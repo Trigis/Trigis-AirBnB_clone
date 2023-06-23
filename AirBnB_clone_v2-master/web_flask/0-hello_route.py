55cfcf19736460e0a585559b4132fd6c88d00002
Emuron Gabriel <76200152+Trigis@users.noreply.github.com>
2023-06-23 11:19:45 +0300

    Update 0-hello_route.py

--- AirBnB_clone_v2-master/web_flask/0-hello_route.py
+++ AirBnB_clone_v2-master/web_flask/0-hello_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
+"""
+Starts a Flask web application.
 """
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def airbnb_onepage():
+    """Display 'AirBnB Clone onepage'"""
+    return 'AirBnB Clone onepage'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
